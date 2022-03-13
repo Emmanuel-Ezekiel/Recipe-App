cadbe85c4a36b074f6486b1778ff04e7f25f2f12
Emmanuel-EEzekiel <emamex44@gmail.com>
2022-03-13 22:59:04 +0100

    add file

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -17,8 +17,17 @@ export class ServerComponent {
     serverId: number = 10;
     serverStatus: string = 'offline'
 
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
+
     // using a method
     getServerStatus() {
         return this.serverStatus;
     }
+
+    getColor() {
+        return this.serverStatus === 'online' ? 'green' : 'red';
+    }
+
 }
